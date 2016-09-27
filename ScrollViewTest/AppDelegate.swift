260333ca4629fbd1350d4c873039ed4d9cbe6bf7
frcocoatst <friedrichhaeupl@friedrichs-macbook-pro.local>
2016-09-27 09:31:18 +0200

    Initial Commit

---
+++ ScrollViewTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScrollViewTest
+//
+//  Created by Friedrich HAEUPL on 27.09.16.
+//  Copyright © 2016 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScrollViewTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScrollViewTest
+//
+//  Created by Friedrich HAEUPL on 27.09.16.
+//  Copyright © 2016 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
